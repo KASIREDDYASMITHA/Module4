56f2974b9cabeb78a945c85c79f650299870a356
KASIREDDYASMITHA <asmithareddy2022@gmail.com>
2026-01-20 20:19:48 +0530

    files

---
+++ expressrouting/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const usersRouter = require("./routes/users.routes");
+const todosRouter = require("./routes/todos.routes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/users", usersRouter);
+app.use("/todos", todosRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
